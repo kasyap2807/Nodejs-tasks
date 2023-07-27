ab333e1ac60c61a18112366aa8f7147f0e5a93f9
Krishna Kasyap <125851796+kasyap2807@users.noreply.github.com>
2023-07-27 12:44:31 +0530

    Add files via upload

--- task4.js
+++ task4.js
@@ -1,13 +1,4 @@
-// let fs = require('fs');
-// let arg = process.argv[2];
-// let arg2 = '.'+process.argv[3];
-// fs.readdir(arg,function readov(err,list){
-//     if(err){console.log('error');}
-//     for(let i=0;i<list.length;i++){
-//     if(list[i].toString().includes(arg2))
-//     console.log(list[i]);
-//     }
-// })
+
 let fs = require('fs');
 let path = require('path');
 let dir = process.argv[2];
